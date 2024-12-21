3c121faa8f17a19d7898e96435a9e9fce31e4b9e
Gedasio Silva <Gedasio.pereira@hotmail.com>
2024-12-21 14:22:05 -0300

    Pousada Secreta

---
+++ Loguin&PaginasDinamicas/inc/usuarios.php
@@ -0,0 +1,17 @@
+<?php
+
+//Usuarios onde vai ficar os dados do usuario
+return [
+    [
+        'usuario' => 'usuario1@gmail.com'
+        'senha' => password_hash('senha001',PASSWORD_DEFAULT)
+    ]
+    [
+        'usuario' => 'usuario2@gmail.com'
+        'senha' => password_hash('senha002',PASSWORD_DEFAULT)
+    ]
+    [
+        'usuario' => 'usuario3@gmail.com'
+        'senha' => password_hash('senha003',PASSWORD_DEFAULT)
+    ]
+]
